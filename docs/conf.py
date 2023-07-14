c913c1b82245b2a39f7b59a98090a5fbef947373
egreuel <122284489+egreuel@users.noreply.github.com>
2023-07-14 17:13:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+autodoc_mock_imports = ['rclpy']
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
